0161c484840c11be5f9a60842c4dcfef2dbf170d
Ricki.Xu <jkxyx205@163.com>
2015-02-08 19:42:11 +0800

    add db script

---
+++ db/dic_oracle.sql
@@ -0,0 +1,13 @@
+-- Create table
+create table RICK_DICTIONRARY
+(
+  id        VARCHAR2(32 CHAR),
+  key       VARCHAR2(32 CHAR),
+  value     VARCHAR2(32 CHAR),
+  truevalue VARCHAR2(32 CHAR),
+  fid       VARCHAR2(32 CHAR),
+  seq       NUMBER
+)
+ 
+
+  
